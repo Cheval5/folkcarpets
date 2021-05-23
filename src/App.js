664d0d190c2cfdf3357bdd392ee9395812da604c
Ali Akbar <63883870+Cheval5@users.noreply.github.com>
2021-05-23 04:17:09 -0400

    app.js changed to class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from './assets/logo/yellow-logo.png';
 import './App.scss';
-
-const App = () => {
-  return (
-    <div className='grid-container'>
-      <div className='grid-container__head'>
-        <button className='grid-container__logo'><img className='grid-container__logo-pic'src={Logo} alt="Folk Carpets Logo" />Folk</button>
-        <p className='grid-container__name'>Carpets</p>
+import data from './data.json';
+class App extends Component {
+  render() {
+    return (
+      <div className='grid-container'>
+        <div className='grid-container__head'>
+          <button className='grid-container__logo'><img className='grid-container__logo-pic'src={Logo} alt="Folk Carpets Logo" />Folk</button>
+          <p className='grid-container__name'>Carpets</p>
+        </div>
+        <div className='grid-container__main'> rug list</div>
+        <div className='grid-container__foot'>All right is reserved</div>
       </div>
-      <div className='grid-container__main'> rug list</div>
-      <div className='grid-container__foot'>All right is reserved</div>
-    </div>
-  );
+    )
+  }
 };
 
 export default App;
