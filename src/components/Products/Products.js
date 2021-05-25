7166842414bb292c197afa6f692289f5ef83f3fa
Ali Akbar <63883870+Cheval5@users.noreply.github.com>
2021-05-24 23:12:02 -0400

    redux filter added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -66,5 +66,5 @@ class Products extends Component {
     }
 }
 
-export default connect((state) => ({products: state.products.items}), {
+export default connect((state) => ({products: state.products.filteredItems}), {
     fetchProducts})(Products);
